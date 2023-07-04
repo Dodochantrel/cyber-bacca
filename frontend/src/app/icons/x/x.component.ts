deb14beefb6843e8c4eff2ded0378c4cad3ad8e0
Chantrel.D <dorian.chantrel@hotmail.fr>
2023-07-04 15:36:06 +0200

    nmap

---
+++ frontend/src/app/icons/x/x.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-x-icon',
+    standalone: true,
+    templateUrl: './x.component.html',
+    styleUrls: ['./x.component.css'],
+})
+
+export class XComponent {
+}
--- frontend/src/app/pages/admin/admin.component.ts
+++ frontend/src/app/pages/admin/admin.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonFormComponent } from 'src/app/components/button_form/button_form.component';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { XComponent } from 'src/app/icons/x/x.component';
 
 @Component({
     selector: 'app-admin',
     standalone: true,
     imports: [
         HeaderComponent,
-        CommonModule
+        CommonModule,
+        XComponent,
+        ButtonFormComponent
     ],
     templateUrl: './admin.component.html',
     styleUrls: ['./admin.component.css'],
@@ -15,8 +19,25 @@ import { HeaderComponent } from 'src/app/components/header/header.component';
 
 export class AdminComponent {
     currentSection: string = 'tool';
+    nmap: boolean = false;
+    waitingNmap: boolean = true;
+    loadingNmap: boolean = false;
+    nmapResult: boolean = false;
 
     showSection(section: string): void {
         this.currentSection = section;
     }
+
+    showForm(form: string): void {
+        switch (form) {
+            case 'nmap':
+                this.nmap = true;
+                break;
+        }
+    }
+
+    close(){
+        this.nmap = false;
+    }
+      
 }
