a7e8e7c258a8106aad71f18a861981301282ed3c
Chantrel.D <dorian.chantrel@hotmail.fr>
2023-07-05 15:41:51 +0200

    same

--- frontend/src/app/components/chart_line/chartLine.component.ts
+++ frontend/src/app/components/chart_line/chartLine.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ChartDataset, ChartOptions } from 'chart.js';
 
 @Component({
     selector: 'app-graph',
@@ -9,12 +8,4 @@ import { ChartDataset, ChartOptions } from 'chart.js';
 })
 
 export class ChartLineComponent {
-    public lineChartData: ChartDataset[] = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      ];
-      public lineChartOptions: ChartOptions = {
-        responsive: true,
-      };
-      public lineChartLegend = true;
-      public lineChartType = 'line';
 }
